971486d8f98c43a007d851b0273686b4811b9f31
Patricia Santana <psantana.student@hsfi.us>
2017-02-01 23:03:48 +0000

    added console.log

--- scripts.js
+++ scripts.js
@@ -4,5 +4,9 @@ $(document).ready(function (){
   var url = "http://api.openweathermap.org/data/2.5/weather?q="
      + city 
      + "units=imperial&appid=d92b1594354e0df5d46c7f5f8bb29a31"
+ $.get(url, function(data){
+    
+    console.log(data)
+ });
    });
 });
