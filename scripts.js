998a2195ee62f073f632ffd278866ff535daaf79
Patricia Santana <psantana.student@hsfi.us>
2017-02-08 22:32:19 +0000

    added a height to the div weather-results, added temp+humidity+wind speed to html, made a variable of data that contained these values

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ $(document).ready(function (){
   var city = $("input").val();
   var url = "http://api.openweathermap.org/data/2.5/weather?q="
      + city 
-     + "units=imperial&appid=d92b1594354e0df5d46c7f5f8bb29a31"
+     + "&units=imperial&appid=d92b1594354e0df5d46c7f5f8bb29a31"
+     console.log(url)
  $.get(url, function(data){
-    
+   var data = data.name + ' ' + data.main.temp + 'F ' + data.main.humidity + '% humidity ' + data.wind.speed + ' mph';
+   $("#weather-results").html(data + "");
     console.log(data)
  });
    });
